df705ae6c551a5cab6075dad35b14dd2728f635d
rubayatrifat <rubayatislam17@gmail.com>
2025-06-04 15:33:31 +0600

    Variables (4th June 2025)

---
+++ 02. Operators (4-Jun-2025)/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a = 9;
+    cout << a << "\n";
+    cout << ++a << "\n";
+    cout << a++ << "\n";
+    cout << a << endl;
+
+    // circle area
+    float radius;
+    cout << "Enter a radius: ";
+    cin >> radius;
+    float area = 3.1415 * radius * radius;
+    cout << area << "\n";
+
+    // printing ascii value
+    char character;
+    cout << "Write a character :";
+    cin >> character;
+    cout << "The ascii value of " << character << " is" << (int)character;
+}
