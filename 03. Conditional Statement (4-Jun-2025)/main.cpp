0b8aad9afd445a723e15faf18a0fcc5ad46b06eb
rubayatrifat <rubayatislam17@gmail.com>
2025-06-04 18:12:43 +0600

    Conditional Statement (4th June 2025)

---
+++ 03. Conditional Statement (4-Jun-2025)/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout << "Enter a number and it will find out odd or even: ";
+    int inputedNumber;
+    cin >> inputedNumber;
+    if (inputedNumber % 2 != 0) {
+        cout << "This is a odd number \n";
+    } else {
+        cout << "This is a even number \n";
+    }
+
+   // int n;
+    //cout << "Give a negative number: ";
+    //cin >> n;
+    //if (n < 0) {
+        //n = -n;
+    //}
+    //cout << n;
+
+}
