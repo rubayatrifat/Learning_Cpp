1690804e54b81bbe171df3caa69da0fb1961f1f6
rubayatrifat <rubayatislam17@gmail.com>
2025-06-04 09:26:30 +0600

    Variables (4th June 2025)

---
+++ 00. Geting Started (3-Jun-2025)/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout << "Hello,, World!";
+    cout << endl;
+    cout << "Hello,, World!" << endl;
+
+    cout << "Hello,, World! \n";
+    cout << "Hello,, World!";
+}
---
+++ 01. Variables (4-Jun-2025)/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a = 5;
+    int b = 8;
+    cout << a + b << "\n";
+
+    int c = 6, d = 5;
+    cout << c - d << endl;
+
+    d = 3;
+    cout << c * d;
+
+
+
+
+
+
+
+}
